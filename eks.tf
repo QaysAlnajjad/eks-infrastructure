bf133e5f3a82861edac2a1da14e1f241cfc13dcc
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-03-13 16:22:02 +0400

    Remove ArgoCD namespace and Helm release
    Removed ArgoCD namespace and Helm release resources.

--- eks.tf
+++ eks.tf
@@ -18,15 +18,3 @@ resource "aws_eks_cluster" "this" {
   ]
 }
 
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
-resource "helm_release" "argocd" {
-  name = "argocd"
-  namespace = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart = "argo-cd"
-}
