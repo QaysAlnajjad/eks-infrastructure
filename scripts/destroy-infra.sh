d8b17ee12f8da5c160ed89e1a3aa0f3a64472315
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-03-13 16:14:23 +0400

    Update destroy-infra.sh

--- scripts/destroy-infra.sh
+++ scripts/destroy-infra.sh
@@ -13,12 +13,12 @@ echo "Waiting for AWS to clean up ENIs..."
 sleep 90
 
 # 2. Terraform
-terraform -chdir="terraform" init -reconfigure -upgrade \
+terraform init -reconfigure -upgrade \
     -backend-config="bucket=$TF_STATE_BUCKET_NAME" \
     -backend-config="key=EKS-project/terraform.tfstate" \
     -backend-config="region=$TF_STATE_BUCKET_REGION"
 
-terraform -chdir="terraform" destroy \
+terraform destroy \
     -var aws_region=$AWS_REGION \
     -var cluster_name=$CLUSTER_NAME \
     -var-file="terraform.tfvars" \
