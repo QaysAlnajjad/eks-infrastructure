8f43d51f67f317d318fae9c52918e0cc647c1b2a
Qays Alnajjad <103290698+QaysAlnajjad@users.noreply.github.com>
2026-03-29 19:52:24 +0400

    Remove aws_eks_cluster_auth data block
    Removed unused aws_eks_cluster_auth data block.

--- iam.tf
+++ iam.tf
@@ -91,9 +91,7 @@ data "aws_eks_cluster" "this" {
   name = aws_eks_cluster.this.name
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
-}
+
 
 resource "aws_iam_openid_connect_provider" "eks" {
   url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
